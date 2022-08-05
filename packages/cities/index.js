985e96770f1dd5984d451597cd1a52bf90ae61e0
Brian Nguyen <vanpho02@gmail.com>
2022-08-05 15:04:49 +0700

    feat: remove other city

--- packages/cities/index.js
+++ packages/cities/index.js
@@ -4,7 +4,6 @@ const CITIES = [
   {
     name: "New York", country: COUNTRIES.US,
     name: "Los Angeles", country: COUNTRIES.US,
-    name: "Saigon", country: COUNTRIES.VN,
     name: "Manila", country: COUNTRIES.PL,
   }
 ];
