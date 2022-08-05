2479e393471612bdda6a976a0525b005e0b612b1
Brian Nguyen <vanpho02@gmail.com>
2022-08-05 15:03:19 +0700

    chore: remove Hanoi

--- packages/cities/index.js
+++ packages/cities/index.js
@@ -4,7 +4,6 @@ const CITIES = [
   {
     name: "New York", country: COUNTRIES.US,
     name: "Los Angeles", country: COUNTRIES.US,
-    name: "Hanoi", country: COUNTRIES.VN,
     name: "Saigon", country: COUNTRIES.VN,
     name: "Manila", country: COUNTRIES.PL,
   }
