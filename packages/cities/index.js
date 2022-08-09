2bbe66263b0b3a68f4a47a00751fcef316f4582e
Brian Nguyen <vanpho02@gmail.com>
2022-08-09 09:25:30 +0700

    feat: add documentation and fix small bug

--- packages/cities/index.js
+++ packages/cities/index.js
@@ -1,11 +1,9 @@
 const { COUNTRIES } = require("@brian-demo-050822/countries")
 
 const CITIES = [
-  {
-    name: "New York", country: COUNTRIES.US,
-    name: "Los Angeles", country: COUNTRIES.US,
-    name: "Manila", country: COUNTRIES.PL,
-  }
+  { name: "New York", country: COUNTRIES.US },
+  { name: "Los Angeles", country: COUNTRIES.US },
+  { name: "Manila", country: COUNTRIES.PL },
 ];
 
 module.exports = { CITIES };
