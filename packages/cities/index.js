192c5860bff31df84a7c56a827c469d8f88e3dd3
Brian Nguyen <vanpho02@gmail.com>
2022-08-11 17:47:50 +0700

    feat: add Hanoi

--- packages/cities/index.js
+++ packages/cities/index.js
@@ -4,6 +4,7 @@ const CITIES = [
   { name: "New York", country: COUNTRIES.US },
   { name: "Los Angeles", country: COUNTRIES.US },
   { name: "Manila", country: COUNTRIES.PL },
+  { name: "Hanoi", country: COUNTRIES.VN },
 ];
 
 module.exports = { CITIES };
